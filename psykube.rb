33b11d67cd2fb80b61b15b25a2acdcf54ba20596
Jason Waldrip <jason@waldrip.net>
2016-12-29 19:42:27 -0700

    Update psykube.rb

--- psykube.rb
+++ psykube.rb
@@ -2,7 +2,8 @@ require "formula"
 
 class Psykube < Formula
   VERSION = "0.1.0-beta2"
-
+  
+  version VERSION
   homepage 'https://github.com/commercialtribe/psykube'
   head 'https://github.com/commercialtribe/psykube.git', branch: 'master'
   url 'https://github.com/commercialtribe/psykube.git', using: :git, tag: "v#{VERSION}"
