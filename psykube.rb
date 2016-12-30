5940a7e16c44b37fffae7557899d316c2c680ea5
Jason Waldrip <jason@waldrip.net>
2016-12-29 17:26:44 -0700

    Create psykube.rb

---
+++ psykube.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class GitGet < Formula
+  VERSION = "0.1.0-beta1"
+
+  version VERSION
+  homepage "https://github.com/commercialtribe/psykube"
+  head "https://github.com/commercialtribe/psykube.git", branch: "master"
+  url "https://github.com/commercialtribe/psykube.git", using: :git, tag: "v#{VERSION}"
+
+  depends_on "crystal" => :build
+
+  def install
+    system "shards install"
+    system "crystal build"
+    bin.install "psykube"
+  end
+  
+end
