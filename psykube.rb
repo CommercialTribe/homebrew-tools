b96b5a50afc7ac5a62baadfb13c10138bcd7b8f5
Jason Waldrip <jason@waldrip.net>
2016-12-29 19:26:16 -0700

    Update psykube.rb

--- psykube.rb
+++ psykube.rb
@@ -4,15 +4,14 @@ class Psykube < Formula
   VERSION = "0.1.0-beta1"
 
   version VERSION
-  homepage "https://github.com/commercialtribe/psykube"
-  head "https://github.com/commercialtribe/psykube.git", branch: "master"
-  url "https://github.com/commercialtribe/psykube.git", using: :git, tag: "v#{VERSION}"
+  homepage 'https://github.com/commercialtribe/psykube'
+  head 'https://github.com/commercialtribe/psykube.git', branch: 'master'
+  url 'https://github.com/commercialtribe/psykube.git', using: :git, tag: "v#{VERSION}"
 
-  depends_on "crystal-lang" => :build
+  depends_on 'crystal-lang' => :build
 
   def install
-    system "shards install"
-    system "crystal build ./src/psykube.cr"
+    system 'make build'
     bin.install "psykube"
   end
   
