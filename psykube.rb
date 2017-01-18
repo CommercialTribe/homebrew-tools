3f7429af62b4777c94c461dff03943c68afcb90e
Jason Waldrip <jason@waldrip.net>
2017-01-18 14:18:56 -0700

    Update psykube.rb

--- psykube.rb
+++ psykube.rb
@@ -1,19 +1,20 @@
+require "net/http"
 require "formula"
 
 class Psykube < Formula
-  VERSION = "0.1.0-beta11"
-  
-  version VERSION
+  TAG = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/commercialtribe/psykube/releases/latest")))["tag_name"]
+
+  version TAG.sub /^v/, ''
   homepage 'https://github.com/commercialtribe/psykube'
   head 'https://github.com/commercialtribe/psykube.git', branch: 'master'
-  url 'https://github.com/commercialtribe/psykube.git', using: :git, tag: "v#{VERSION}"
+  url 'https://github.com/commercialtribe/psykube.git', using: :git, tag: TAG
 
   depends_on 'crystal-lang' => :build
 
   def install
-    ENV["TRAVIS_TAG"] = "v#{VERSION}" unless build.head?
-    system 'make build'
-    bin.install "psykube"
+    ENV["TRAVIS_TAG"] = TAG unless build.head?
+    system 'shards build'
+    bin.install "bin/psykube"
   end
-  
+
 end
