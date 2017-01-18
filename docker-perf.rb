6b2d2a84e175aba02a8628e3253ad492a4943360
Jason Waldrip <jason@waldrip.net>
2017-01-18 15:53:07 -0700

    Update docker-perf.rb

--- docker-perf.rb
+++ docker-perf.rb
@@ -6,12 +6,23 @@ class DockerPerf < Formula
   depends_on 'docker'
 
   def install
-    Dir.chdir File.expand_path("/Users/#{ENV['USER']}/Library/Containers/com.docker.docker/Data/database/") do
-      filename = "com.docker.driver.amd64-linux/disk/full-sync-on-flush"
-      system 'git reset --hard'
-      File.open(filename, "w"){ |file| file.write "true\n" }
-      system "git add #{filename}"
-      system 'git commit -s -m "Disable flushing"'
+    File.open("docker-perf", "w") do |file|
+      file.write <<~BASH
+        #!/bin/bash
+        # From: https://gist.github.com/kortina/67ad6e40e40d5199c3507cdad0c9a12c
+        # Tune fsync perf for docker mac per
+        # https://github.com/docker/for-mac/issues/668
+        # NB: You must be on Docker 1.12.2-rc1-beta27 or greater
+
+        cd ~/Library/Containers/com.docker.docker/Data/database/
+        f="com.docker.driver.amd64-linux/disk/full-sync-on-flush"
+        git reset --hard
+        cat $f
+        echo "false" > $f
+        git add $f && git commit -s -m "Disable flushing"
+        echo "You should now restart Docker for Mac."
+      BASH
     end
+    bin.install "docker-perf"
   end
 end
