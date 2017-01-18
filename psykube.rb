f530ecd521ceb730183a878def8723a1a1954558
Jason Waldrip <jason@waldrip.net>
2017-01-18 14:53:20 -0700

    Update psykube.rb

--- psykube.rb
+++ psykube.rb
@@ -2,7 +2,9 @@ require "net/http"
 require "formula"
 
 class Psykube < Formula
-  TAG = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/commercialtribe/psykube/releases/latest")))["tag_name"]
+  LATEST_RELEASE = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/commercialtribe/psykube/releases/latest")))
+  FALLBACK_TAG = "v0.1.0-beta11"
+  TAG = LATEST_RELEASE["tag_name"] || FALLBACK_TAG
 
   version TAG.sub /^v/, ''
   homepage 'https://github.com/commercialtribe/psykube'
@@ -10,11 +12,12 @@ class Psykube < Formula
   url 'https://github.com/commercialtribe/psykube.git', using: :git, tag: TAG
 
   depends_on 'crystal-lang' => :build
+  depends_on 'kubernetes-cli'
+  depends_on 'docker'
 
   def install
     ENV["TRAVIS_TAG"] = TAG unless build.head?
     system 'shards build'
     bin.install "bin/psykube"
   end
-
 end
